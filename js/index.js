8571077307b696920e28326ef01478658dfa75ac
hyeona <01.hyeona@gmail.com>
2026-08-27 09:00:49 +0900

    feat : header 메뉴/닫기 로직 추가

---
+++ js/index.js
@@ -0,0 +1,43 @@
+const btnMenu = document.querySelector('.btn-menu');
+const smartOverlayMenu = document.querySelector('.smart-overlay-menu');
+const btnClose = document.querySelector('.btn-close');
+
+// 오버레이 열고 닫는 기능 
+if (btnMenu) { // btnMenu에 .btn-menu 클래스가 저장됐을 때 
+    // 열기
+    // 버튼을 눌렀을 때 오버레이가 나와야함 
+    btnMenu.addEventListener('click', function() {
+        smartOverlayMenu.classList.add('on');
+    });   
+}    
+else { 
+    alert('btn-menu 클래스가 없어요.')
+}
+
+
+if (btnClose) {
+    // 닫기 
+    // 버튼을 닫았을 때 오버레이가 닫혀야 함
+    btnClose.addEventListener('click', function() {
+        smartOverlayMenu.classList.remove('on');
+    });
+}
+else { 
+    alert('btn-close 클래스가 없어요.')
+}
+
+// 뎁스 영역
+const gnbSmartList = document.querySelectorAll('.gnb-smart>li');
+const gnb2DepthsList = document.querySelectorAll('.gnb2depths-smart');
+gnbSmartList.forEach((li, idx) => {
+    li.addEventListener('click', function(e) {
+        e.preventDefault();
+
+        // 색상 변경
+        gnbSmartList.forEach(litag=>litag.classList.remove('on'));
+        li.classList.add('on');
+    
+        gnb2DepthsList.forEach(div=>div.classList.remove('on'));
+        gnb2DepthsList[idx].classList.add('on');
+    })
+});
