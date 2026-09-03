1b09ab7b3fed7acab7727ca4f6c1db903d283b99
hyeona <01.hyeona@gmail.com>
2026-09-04 00:45:03 +0900

    feat: 상품 리스트 안에 있는 상품 렌더링 될 데이터

---
+++ js/buy-product.js
@@ -0,0 +1,134 @@
+let buyProductArray = [
+    {
+        pid: 1,
+        pmenu: '테이블',
+        pname: '루버 오발 원목 식탁 (6인용)',
+        pdesc: '입체적인 템바보드 다리와 부드러운 라운드 상판이 돋보이는 프리미엄 오크 원목 테이블',
+        pprice: 100000,
+        pdiscount: 0.3,
+        saleprice: 70000,
+        heart: 892,
+        pimg: 'list-1.png'
+    },
+    {
+        pid: 2,
+        pmenu: '의자',
+        pname: '포레스트 패브릭 다이닝 체어',
+        pdesc: '편안한 곡선과 포근한 패브릭으로 식사 공간을 따뜻하게 만들어주는 의자',
+        pprice: 150000,
+        pdiscount: 0.2,
+        saleprice: 120000,
+        heart: 246,
+        pimg: 'list-2.png'
+    },
+    {
+        pid: 3,
+        pmenu: '조명',
+        pname: '우든 세이프 펜던트 조명',
+        pdesc: '단정한 형태와 따뜻한 빛으로 공간을 편안하게 밝혀주는 펜던트 조명',
+        pprice: 180000,
+        pdiscount: 0.2,
+        saleprice: 144000,
+        heart: 12,
+        pimg: 'list-3.png'
+    },
+    {
+        pid: 4,
+        pmenu: '수납장',
+        pname: '미니멀 오크 3단 서랍장',
+        pdesc: '군더더기 없는 디자인과 넉넉한 수납 공간을 갖춘 원목 서랍장',
+        pprice: 420000,
+        pdiscount: 0.3,
+        saleprice: 294000,
+        heart: 128,
+        pimg: 'list-4.png'
+    },
+    {
+        pid: 5,
+        pmenu: '데코',
+        pname: '매트 세라믹 화병 (S/M)',
+        pdesc: '오목한 내추럴 쉐입과 담백한 색감이 돋보이는 세라믹 화병',
+        pprice: 100000,
+        pdiscount: 0.25,
+        saleprice: 75000,
+            heart: 54,
+        pimg: 'list-5.png'
+    },
+    {
+        pid: 6,
+        pmenu: '조명',
+        pname: '세라믹 픽스 스탠드 조명',
+        pdesc: '도자기 특유의 자연스러운 질감과 은은한 조도가 어우러진 스탠드 조명',
+        pprice: 240000,
+        pdiscount: 0.3,
+        saleprice: 168000,
+        heart: 88,
+        pimg: 'list-6.png'
+    },
+    {
+        pid: 7,
+        pmenu: '수납장',
+        pname: '슬라임 원목 사이드 콘솔',
+        pdesc: '부드러운 라인과 충분한 수납으로 공간을 정돈해주는 원목 콘솔',
+        pprice: 520000,
+        pdiscount: 0.2,
+        saleprice: 416000,
+        heart: 41,
+        pimg: 'list-7.png'
+    },
+    {
+        pid: 8,
+        pmenu: '데코',
+        pname: '센티드 우드 디퓨저 세트',
+        pdesc: '은은한 우디 향과 감각적인 용기가 어우러진 디퓨저 세트',
+        pprice: 70000,
+        pdiscount: 0.15,
+        saleprice: 59500,
+            heart: 943,
+        pimg: 'list-8.png'
+    },
+    {
+        pid: 9,
+        pmenu: '패브릭',
+        pname: '내추럴 린넨 원단 테이블보',
+        pdesc: '자연스러운 질감과 차분한 색감으로 식탁을 완성하는 린넨 테이블보',
+        pprice: 88000,
+        pdiscount: 0.2,
+        saleprice: 70400,
+        heart: 62,
+        pimg: 'list-9.png'
+    },
+    {
+        pid: 10,
+        pmenu: '패브릭',
+        pname: '텍스처 롱 러그',
+        pdesc: '포근한 촉감과 잔잔한 패턴으로 공간에 편안함을 더하는 러그',
+        pprice: 210000,
+        pdiscount: 0.2,
+        saleprice: 168000,
+            heart: 350,
+        pimg: 'list-10.png'
+    },
+    {
+        pid: 11,
+        pmenu: '가구',
+        pname: '아치형 원목 패브릭 수납 침대 (Q)',
+        pdesc: '헤드보드와 넉넉한 수납을 한 번에 해결하는 원목 침대',
+        pprice: 1200000,
+        pdiscount: 0.3,
+        saleprice: 840000,
+        heart: 76,
+    pimg: 'list-11.png'
+    },
+    {
+        pid: 12,
+        pmenu: '가구',
+        pname: '클라우드 패브릭 3인 소파',
+        pdesc: '부드러운 패브릭과 넉넉한 쿠션으로 편안한 휴식을 제공하는 소파',
+        pprice: 950000,
+        pdiscount: 0.15,
+        saleprice: 807500,
+        heart: 150,
+        pimg: 'list-12.png'
+    }
+];
